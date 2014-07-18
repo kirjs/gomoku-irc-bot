c496c0b8c561a04d65e0f1d5cde4d32d99776b03
Kirill Cherkashin <kirill.cherkashin@lab49.com>
2014-07-17 23:00:00 -0400

    Add spec for the game registration handler

---
+++ __tests__/plugins/registerSpec.js
@@ -0,0 +1,19 @@
+jest.dontMock('../../lib/plugins/register');
+var register = require('../../lib/plugins/register');
+
+describe('register state', function () {
+    beforeEach(function () {
+        this.registerHandler = new register();
+    });
+
+    it('Allows to register a player', function () {
+        expect(this.registerHandler.play('mememe')).toContain('mememe');
+    });
+    it('Allows to register more then one player', function () {
+        expect(this.registerHandler.play('bebe')).toContain('bebe');
+        expect(this.registerHandler.play('mememe')).toContain('mememe');
+    });
+
+
+});
+
