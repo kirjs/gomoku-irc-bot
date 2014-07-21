7fadd8be8f8db17f570521e9a30b1f1d7a546c11
Kirill Cherkashin <kirill.cherkashin@lab49.com>
2014-07-21 17:51:45 -0400

    Implement changing state of the game

--- __tests__/plugins/playSpec.js
+++ __tests__/plugins/playSpec.js
@@ -1,4 +1,5 @@
 jest.dontMock('../../lib/plugins/play');
+jest.dontMock('gomoku-tools');
 var Play = require('../../lib/plugins/play');
 
 describe('Play state', function () {
@@ -7,7 +8,25 @@ describe('Play state', function () {
     });
 
     it('Allows to change the state manually', function () {
-        expect(this.playHandler.$default('h2')).toBe('position');
+
+        var pos =
+          '     A  B  C  D  E  F  G  H  I  J  K  L  M  N  O \n'
+        + ' 15                                              \n'
+        + ' 14                                              \n'
+        + ' 13                                              \n'
+        + ' 12                                              \n'
+        + ' 11                                              \n'
+        + ' 10                                              \n'
+        + ' 9                                               \n'
+        + ' 8                                               \n'
+        + ' 7                                               \n'
+        + ' 6                                               \n'
+        + ' 5                                               \n'
+        + ' 4                                               \n'
+        + ' 3                                               \n'
+        + ' 2                        x                      \n'
+        + ' 1                                               \n';
+        expect(this.playHandler.$default('h2')).toBe(pos );
     });
 
 
