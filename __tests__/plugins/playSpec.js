bcc105e15c9334f25734597d9bb1a2525c578c00
Kirill Cherkashin <kirill.cherkashin@lab49.com>
2014-07-21 22:31:47 -0400

    Apparently it is not  a good idea to output the ascii version of the game, because irc doesn't allow to use monospace font. So we're moving to a more hardcore version where a player will only see move coordinates

--- __tests__/plugins/playSpec.js
+++ __tests__/plugins/playSpec.js
@@ -8,25 +8,7 @@ describe('Play state', function () {
     });
 
     it('Allows to change the state manually', function () {
-
-        var pos =
-          '     A  B  C  D  E  F  G  H  I  J  K  L  M  N  O \n'
-        + ' 15                                              \n'
-        + ' 14                                              \n'
-        + ' 13                                              \n'
-        + ' 12                                              \n'
-        + ' 11                                              \n'
-        + ' 10                                              \n'
-        + ' 9                                               \n'
-        + ' 8                                               \n'
-        + ' 7                                               \n'
-        + ' 6                                               \n'
-        + ' 5                                               \n'
-        + ' 4                                               \n'
-        + ' 3                                               \n'
-        + ' 2                        x                      \n'
-        + ' 1                                               \n';
-        expect(this.playHandler.$default('h2')).toBe(pos );
+        expect(this.playHandler.$default('h2')).toBe('H2');
     });
 
 
